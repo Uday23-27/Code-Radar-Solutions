3ef465b956fa91553d8de9c2c24722c02e1dd8c1
Uday23-27 <udaygoyal2327@gmail.com>
2025-02-15 22:21:42 +0530

    Solution to Check Triangle Validity

---
+++ Check Triangle Validity@@6788dd63490637cfc44e1b93/code.c
@@ -0,0 +1,32 @@
+// Your code here...
+#include <stdio.h>
+#include <math.h>
+int main(){
+    int Side1;
+    int Side2;
+    int Side3;
+
+    scanf("%d", &Side1);
+    scanf("%d", &Side2);
+    scanf("%d", &Side3);
+
+    int Power1 = pow(Side1, 2);
+    int Power2 = pow(Side2, 2);
+    int Power3 = pow(Side3, 2);
+
+    if (Power1 > Power2 && Power1 > Power3){
+        if (Power1 = Power2 + Power3){
+            printf("Valid");
+        }
+    }
+    if (Power2 > Power1 && Power2 > Power3){
+        if (Power2 = Power1 + Power3){
+            printf("Valid");
+        }
+    }
+    if (Power3 > Power2 && Power3 > Power1){
+        if (Power3 = Power2 + Power1){
+            printf("Valid");
+        }
+    }
+}
