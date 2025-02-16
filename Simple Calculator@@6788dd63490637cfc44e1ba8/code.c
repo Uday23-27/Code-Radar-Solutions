8d03566677188d4c83f883ccd09bdc8634f2b76b
Uday23-27 <udaygoyal2327@gmail.com>
2025-02-16 14:41:02 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -1,4 +1,29 @@
 // Your code here...
+// #include <stdio.h>
+
+// int main(){
+//     int Num1;
+//     int Num2;
+//     char ch;
+
+//     scanf("%d", &Num1);
+//     scanf("%d", &Num2);
+//     scanf("%c", &ch);
+
+//     if (ch == '+'){
+//         printf("%d",Num1 + Num2);
+//     }
+//     else if (ch == '-'){
+//         printf("%d",Num1 - Num2);
+//     }
+//     else if (ch == '*'){
+//         printf("%d",Num1 * Num2);
+//     }
+//     else if (ch == '/'){
+//         printf("%d",Num1 / Num2);
+//     }
+// }
+
 #include <stdio.h>
 
 int main(){
@@ -8,18 +33,26 @@ int main(){
 
     scanf("%d", &Num1);
     scanf("%d", &Num2);
-    scanf("%c", &ch);
+    scanf(" %c", &ch); // Add a space before %c to ignore leading whitespace
 
     if (ch == '+'){
-        printf("%d",Num1 + Num2);
+        printf("%d", Num1 + Num2);
     }
     else if (ch == '-'){
-        printf("%d",Num1 - Num2);
+        printf("%d", Num1 - Num2);
     }
     else if (ch == '*'){
-        printf("%d",Num1 * Num2);
+        printf("%d", Num1 * Num2);
     }
     else if (ch == '/'){
-        printf("%d",Num1 / Num2);
+        if (Num2 != 0) {
+            printf("%d", Num1 / Num2);
+        } else {
+            printf("Error: Division by zero\n");
+        }
+    } else {
+        printf("Invalid operator\n");
     }
+
+    return 0;
 }
